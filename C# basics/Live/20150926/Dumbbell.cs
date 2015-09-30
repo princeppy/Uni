0391f0af55a572c33fe17a70c93f8ec0f399bc01
vesheff <martin.veshev@gmail.com>
2015-10-01 00:02:05 +0300

    Added live exercises

---
+++ C# basics/Live/20150926/Dumbbell.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+
+namespace Dumbbell
+{
+    class Dumbbell
+    {
+        static void Main(string[] args)
+        {
+            // Dumbbell
+            // Link: https://judge.softuni.bg/Contests/Practice/Index/78#2
+
+            int size = int.Parse(Console.ReadLine());
+
+            string dotsFront = new string('.', size / 2);
+            string symbols = new string('&', size / 2 + 1);
+            string dotsMiddle = new string('.', size);
+
+            // Uppermost row
+            Console.WriteLine("{0}{1}{2}{1}{0}", dotsFront, symbols, dotsMiddle);
+
+
+            string dots = new string('.', dotsFront.Count() - 1);
+            string stars = new string('*', size / 2);
+
+            // Upper part
+            for (int i = 0; i < size / 2; i++)
+            {
+                if (i == (size / 2 - 1))
+                {
+                    string handle = new string('=', size);
+                    Console.WriteLine("{0}{1}{2}{1}{3}{1}{2}{1}{0}", dots, "&", stars, handle);
+                    dots = new string('.', 0);
+                }
+                else
+                {
+                    Console.WriteLine("{0}{1}{2}{1}{3}{1}{2}{1}{0}", dots, "&", stars, dotsMiddle);
+                    dots = new string('.', dots.Count() - 1);
+                }
+
+                stars = new string('*', stars.Count() + 1);
+            }
+
+            // Lower part
+            dots = new string('.', 1);
+            stars = new string('*', stars.Count() - 2);
+
+            for (int i = 0; i < size / 2 - 1; i++)
+            {
+                Console.WriteLine("{0}{1}{2}{1}{3}{1}{2}{1}{0}", dots, "&", stars, dotsMiddle);
+
+                dots = new string('.', dots.Count() + 1);
+                stars = new string('*', stars.Count() - 1);
+            }
+
+            // Lowermost row
+            Console.WriteLine("{0}{1}{2}{1}{0}", dotsFront, symbols, dotsMiddle);
+        }
+    }
+}
---
+++ C# basics/Live/20150926/KingOfThieves.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LiveDemos
+{
+    class KingOfThieves
+    {
+        // King of thieves
+        // Link: https://judge.softuni.bg/Contests/Practice/Index/81#2
+        private static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+
+            int dashesCount = size / 2;
+            int symbolCount = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                string dashes = new string('-', dashesCount);
+                string symbols = new string(symbol, symbolCount);
+                Console.WriteLine("{0}{1}{0}", dashes, symbols);
+
+                if (i < size / 2)
+                {
+                    dashesCount--;
+                    symbolCount += 2;
+                }
+                else
+                {
+                    dashesCount++;
+                    symbolCount -= 2;
+                }
+            }
+        }
+    }
+}
